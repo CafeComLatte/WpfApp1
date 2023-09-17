8dc33f28ccd9810bf0ebce7e15cf365d6940b20e
lee <asdf@LAPTOP-94UDFS0G>
2023-09-17 11:27:45 +0900

    Purchase Page Work

---
+++ Common/control/ReciptItemControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Common.control
+{
+    public class ReciptItemControl : Button
+    {
+        public ReciptItemControl() { 
+            base.DefaultStyleKey = typeof(ReciptItemControl);   
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+
+    }
+}
---
+++ Common/converter/InputStrConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Common.converter
+{
+    public class InputStrConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value.ToString();
+            string formatedStr = str.Substring(0, 4) + "/" + str.Substring(4,2) + "/" + str.Substring(6,2);
+            Console.WriteLine(formatedStr); 
+            return formatedStr;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
